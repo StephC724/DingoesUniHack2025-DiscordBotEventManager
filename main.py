0859b79925d90e8e591e05b5adf9481850672ef3
fenrir51 <aelnick@hotmail.com>
2025-03-15 17:03:24 +1100

    Removed redundant functions

--- main.py
+++ main.py
@@ -94,32 +94,6 @@ async def start_tracking(channel, duration):
     else:
         results = "\n".join([f"<@{user_id}>: {count} messages" for user_id, count in message_count.items()])
         await channel.send(f"Message count after {duration} seconds:\n{results}")
-    
-async def create_event(message: Message):
-    """
-    Event message format: 
-    /em create_event, name, location, start_date, start_time, end_date, end_time
-
-    Creates an event on the Discord server when user sends above formatted message
-    """
-    message_str = message.content
-    name, location, start_d, start_t, end_d, end_t = message_str.split(", ")[1:]
-    start_datetime = convert_to_datetime(start_d, start_t)
-    end_datetime = convert_to_datetime(end_d, end_t)
-    duration = end_datetime - start_datetime
-    await discord.Guild.create_scheduled_event(self=message.guild, name=name, location=location, start_time=start_datetime, end_time=end_datetime, entity_type=discord.EntityType.external, privacy_level=discord.PrivacyLevel.guild_only)
-    start_tracking(message.channel, duration)
-
-def convert_to_datetime(date, time):
-    day, month, year = [int(i) for i in date.split("/")]
-    hour, minute = [int(i) for i in time.split(":")]
-    dt = datetime.datetime(year=year, month=month, day=day, hour=hour, minute=minute)
-    dt = dt.replace(tzinfo=datetime.timezone.utc)
-    return dt
-
-
-async def get_events(server):
-    return await discord.Guild.fetch_scheduled_events(guild=server)
 
 
 
