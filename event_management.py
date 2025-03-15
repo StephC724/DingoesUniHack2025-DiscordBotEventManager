ce7ec110827befff1b636efde7cdccfa690f9627
fenrir51 <aelnick@hotmail.com>
2025-03-15 17:29:32 +1100

    Current event functions
    Also a bit of cleanup of debug messages, as well as adding some type hints around function parameters in event_management.py

--- event_management.py
+++ event_management.py
@@ -21,7 +21,7 @@ async def create_event(message: Message):
     await discord.Guild.create_scheduled_event(self=message.guild, name=name, location=location, start_time=start_datetime, end_time=end_datetime, entity_type=discord.EntityType.external, privacy_level=discord.PrivacyLevel.guild_only)
 
 
-def convert_to_datetime(date, time):
+def convert_to_datetime(date:str, time:str):
     day, month, year = [int(i) for i in date.split("/")]
     hour, minute = [int(i) for i in time.split(":")]
     dt = datetime.datetime(year=year, month=month, day=day, hour=hour, minute=minute)
@@ -29,10 +29,22 @@ def convert_to_datetime(date, time):
     return dt
 
 
-async def get_events(server):
+async def get_events(server:discord.Guild):
     return await discord.Guild.fetch_scheduled_events(guild=server)
 
 
-if __name__ == "__main__":
-    main()
-    
+async def get_current_events(server:discord.Guild):
+    all_events = await get_events(server)
+    now = datetime.utcnow()
+    current_events = []
+    for event in all_events:
+        if is_current_event(event, now):
+            current_events.append(event)
+    return current_events
+
+
+def is_current_event(event:discord.ScheduledEvent, now:datetime):
+    if event.start_time <= now <= event.end_time:
+        return True
+    else:
+        return False
