4c10b4bafea6a01b5857c8c735c6409ba3b9b473
ChrisKH2 <christienaung@outlook.com>
2025-03-15 21:38:23 +1100

    9:38pm - pointSystem.py

--- pointSystem.py
+++ pointSystem.py
@@ -8,7 +8,7 @@ class pointSystem():
 
     serversKey = "Servers"
     usersKey = "Users"
-    pointsKey = "points" # Set a customizable key for the points
+    pointsKey = "UserPoints" # Set a customizable key for the points
 
     def __init__(self):
 
@@ -71,7 +71,10 @@ class pointSystem():
             self.addNewUser(serverID,userID)
         # check if user already exists if they do add points if not add both
 
-        self.pointDatabase[self.serversKey][serverID][self.usersKey][userID][self.pointsKey] -= points_amount
+        usersPoints = self.pointDatabase[self.serversKey][serverID][self.usersKey][userID][self.pointsKey]
+        usersPoints -= points_amount
+        if (usersPoints - points_amount) < 0:
+            usersPoints = 0
 
 
     #used to give a user a whole servers leaderboard
